d2fbc17569c97397179eeef5c4802134b4bab3f0
Joel Carlson <jnkcarlson@gmail.com>
2018-12-20 13:24:16 -0800

    remove extra line

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const
   app = express().use(bodyParser.json()); // creates express http server
 
 
-
-app = express().use(body_parser.json());
-
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
