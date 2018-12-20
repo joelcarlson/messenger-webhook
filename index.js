7667f97dda107db2b18a6450b6fea5f6e04b95cb
Joel Carlson <jnkcarlson@gmail.com>
2018-12-20 13:28:13 -0800

    add pat

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "qs": { "access_token": "EAAQMZAJaWNXsBAMqCZBMey8ZASW7AXZBWaKsrNJEldyx7OFgogiG7dnleAUxgBeZClwYTF7DDGbxLv2br92U1VI5vZBxjdy6oS0gX5zwlcFl8PiVbYFLc3FZCNP4mw8tqoVdgjYZBEalRJbm8JEuyOWs7YZB0Rt8g2GTaECONu7HkegZDZD" },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
